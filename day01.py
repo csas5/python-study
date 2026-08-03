c07f704d17bacaa0f7c200d432103d84dbfd96e0
csas5 <3028844211@qq.com>
2026-08-03 17:34:53 +0800

    完成Python Day1学习

---
+++ day01.py
@@ -0,0 +1,13 @@
+# Day1 Python学习
+
+print("开始学习Python")
+
+name = input("请输入你的名字：")
+
+age = input("请输入你的年龄：")
+
+print("你好,", name)
+
+print("你今年", age, "岁")
+
+print("Python学习正式开始")
---
+++ practice01.py
@@ -0,0 +1,10 @@
+#输入：
+#姓名
+#学校
+#专业
+name=input("plase input your name:")
+school=input("plase input your school name")
+major=input("plase input your major")
+print("name=",name)
+print("school=",school)
+print("major=",major)            
