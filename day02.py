855dc929f6d20a03d208d45aab49fdbbfa200fa5
csas5 <3028844211@qq.com>
2026-08-04 15:36:20 +0800

    already done day2

---
+++ day02.py
@@ -0,0 +1,11 @@
+# Day2：个人信息卡
+
+name = input("请输入你的姓名：")
+age = input("请输入你的年龄：")
+city = input("请输入所在城市：")
+
+print()
+print("===== 个人信息 =====")
+print("姓名：", name)
+print("年龄：", age)
+print("城市：", city)
